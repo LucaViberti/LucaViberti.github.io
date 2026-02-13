323cc84438a93779148f93a96c45df994e4735ff
LucaViberti <lucaviberti@icloud.com>
2026-02-13 20:31:57 +0000

    Remove temp script

--- _git_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /workspaces/LucaViberti.github.io
-git add html/nav.html de/html/nav.html ja/html/nav.html ko/html/nav.html tc/html/nav.html vi/html/nav.html zh/html/nav.html
-git commit -m "Fix Heroes flyout overlap so Pets remains visible"
-git pull --rebase origin main
-git push origin main
-rm -- "$0"
